5e1c388d13b302ceaee4ab9444ce5d115334f0f7
JCesar25 <cruzlino021@gmail.com>
2024-01-30 02:04:06 -0400

    first commit11

--- src/Componentes/Juegos.js
+++ src/Componentes/Juegos.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useSpring, animated } from 'react-spring';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
+import { Link } from "react-router-dom";
 
-// Importa las imágenes necesarias
-import juegosImage1 from '../assets/juegos.jpg';
-import juegosImage2 from '../assets/juegos1.jpg';
-import juegosImage3 from '../assets/juegos2.png';
-import juegosImage4 from '../assets/juegos3.jpg';
-import juegosImage5 from '../assets/juegos4.png';
+import juegosImage1 from "../assets/ARIMETICA.png";
+import juegosImage2 from "../assets/TerminoSemejante.png";
+import juegosImage3 from "../assets/expresionAlgebraica.png";
+import juegosImage4 from "../assets/ecuacion1ergrado.png";
+import juegosImage5 from "../assets/ecuacion2dogrado.png";
 
 const MathExercise = () => {
   const [respuestaCorrecta, setRespuestaCorrecta] = useState(null);
@@ -19,51 +18,66 @@ const MathExercise = () => {
   const [contadorCorrectas, setContadorCorrectas] = useState(0);
   const [contadorIncorrectas, setContadorIncorrectas] = useState(0);
   const [habilitarSelecciones, setHabilitarSelecciones] = useState(true);
+  const [Opciones,setOpciones] = useState(true);
 
   const ejercicios = [
     {
       id: 1,
+      nombre: "Arimetica",
       imagen: juegosImage1,
-      respuestas: [4, 7, 100],
-      respuestaCorrecta: 7,
+      respuestas: [8, 16, 24],
+      respuestaCorrecta: 8,
     },
     {
       id: 2,
+      nombre: "Terminos_Semejantes",
       imagen: juegosImage2,
-      respuestas: [15, 20, 252],
-      respuestaCorrecta: 20,
+      respuestas: ["2a", "4a", "Ninguna de las anteriores"],
+      respuestaCorrecta: "2a",
     },
     {
       id: 3,
+      nombre: "Expresiones_Algebraica",
       imagen: juegosImage3,
-      respuestas: [5, 8, 121 ],
-      respuestaCorrecta: 12,
+      respuestas: [16, 24, 1024],
+      respuestaCorrecta: 16,
     },
     {
       id: 4,
+      nombre: "Ecuacion_1er_Grado",
       imagen: juegosImage4,
-      respuestas: [6, 9, 15],
-      respuestaCorrecta: 9,
+      respuestas: ["1/3", "1/9", "Ninguna de las anteriores"],
+      respuestaCorrecta: "1/3",
     },
     {
       id: 5,
+      nombre: "Ecuacion_2do_Grado",
       imagen: juegosImage5,
-      respuestas: [3, 6, 9],
-      respuestaCorrecta: 6,
+      respuestas: ["x1:1/2, x2:3", "Ninguna de las anteriores", "x1:1/2, x2:3"],
+      respuestaCorrecta: "Ninguna de las anteriores",
     },
   ];
 
   const [animacionRespuesta, setAnimacionRespuesta] = useSpring(() => ({
     opacity: 0,
-    transform: 'translate(-50%, -50%)',
+    transform: "translate(-50%, -50%)",
   }));
 
   const [titleAnimation, setTitleAnimation] = useSpring(() => ({
     opacity: 0,
-    transform: 'translateY(-50px)',
-    border: '2px solid #ddd', // Bordes al título
-    padding: '10px', // Espaciado interno al título
+    transform: "translateY(-50px)",
+    border: "2px solid #ddd",
+    padding: "10px",
   }));
+
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
 
   const iniciarNuevoEjercicio = () => {
     setRespuestaCorrecta(null);
@@ -74,27 +88,27 @@ const MathExercise = () => {
       const nuevoEjercicio = ejercicios[ejercicioActual];
       setEjercicioActual(ejercicioActual + 1);
       setRespuestaCorrecta(nuevoEjercicio.respuestaCorrecta);
+      // Mezclar las respuestas antes de asignarlas
+      const opcionesMezcladas = shuffleArray(
+        nuevoEjercicio.respuestas.map((opcion, index) => ({ id: index, opcion }))
+      );
+      setOpciones(opcionesMezcladas);
     } else {
-      // Todos los ejercicios han sido completados
       setMostrarResumen(true);
     }
   };
-
   const handleButtonClick = (opcion) => {
     if (seleccionada !== null || !habilitarSelecciones) {
-      // Si ya se ha seleccionado una respuesta o las selecciones están deshabilitadas, no hacer nada.
       return;
     }
 
     const respuestaEsCorrecta = opcion === respuestaCorrecta;
 
-    // Configurar animación de respuesta
     setAnimacionRespuesta({
       opacity: 1,
-      transform: 'translate(-50%, -50%)',
+      transform: "translate(-50%, -50%)",
       reset: true,
       onRest: () => {
-        // Actualizar estadísticas después de la animación
         if (respuestaEsCorrecta) {
           setRespuestasCorrectas(respuestasCorrectas + 1);
           setContadorCorrectas(contadorCorrectas + 1);
@@ -103,43 +117,35 @@ const MathExercise = () => {
           setContadorIncorrectas(contadorIncorrectas + 1);
         }
 
-        // Mostrar animación de respuesta correcta o incorrecta
         setTimeout(() => {
           setAnimacionRespuesta({
             opacity: 0,
-            transform: 'translate(-50%, -50%)',
+            transform: "translate(-50%, -50%)",
             reset: true,
           });
 
-          // Iniciar nuevo ejercicio después de la animación
           iniciarNuevoEjercicio();
-        }, 100); // Cambia el tiempo según tus preferencias
+        }, 100);
       },
     });
 
-    // Deshabilitar selecciones después de hacer clic en una respuesta
     setHabilitarSelecciones(false);
   };
 
-  const rotateAnimation = useSpring({
-    transform: `rotate(${seleccionada ? '3deg' : '0deg'})`,
-    config: { duration: 100 },
-  });
-
   const buttonAnimation = useSpring({
-    backgroundColor: seleccionada === respuestaCorrecta ? '#45a049' : '#ff5252',
+    backgroundColor: seleccionada === respuestaCorrecta ? "#45a049" : "#ff5252",
     config: { duration: 100 },
   });
 
   const obtenerCategoria = (porcentaje) => {
     if (porcentaje < 25) {
-      return 'Muy Mal';
+      return "Muy Mal";
     } else if (porcentaje < 50) {
-      return 'Mal';
+      return "Mal";
     } else if (porcentaje < 75) {
-      return 'Bien';
+      return "Bien";
     } else {
-      return 'Excelente';
+      return "Excelente";
     }
   };
 
@@ -163,88 +169,98 @@ const MathExercise = () => {
 
   const styles = {
     container: {
-      position: 'relative',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-      backgroundColor: '#333', // Fondo oscuro
-      color: 'white', // Texto blanco
-      padding: '20px',
-      overflow: 'auto', // Añade un scroll si es necesario
+      position: "relative",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      backgroundColor: "#333",
+      color: "white",
+      padding: "20px",
+      overflow: "auto",
     },
     title: {
       margin: "50px",
-      fontSize: '2em',
-      fontWeight: 'bold',
-      marginBottom: '20px',
+      fontSize: "2em",
+      fontWeight: "bold",
+      marginBottom: "20px",
       ...titleAnimation,
-      textAlign: 'center', // Alineación centrada al título
+      textAlign: "center",
     },
     image: {
-      maxWidth: '100%',
-      border: '2px solid #ddd',
-      borderRadius: '8px',
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-      marginBottom: '20px',
-      ...rotateAnimation,
+      maxWidth: "100%",
+      border: "2px solid #ddd",
+      borderRadius: "8px",
+      boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
     },
     optionsContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      width: '100%',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%",
     },
     optionButton: {
-      padding: '15px 30px',
-      fontSize: '18px',
-      cursor: 'pointer',
-      border: 'none',
-      borderRadius: '5px',
-      margin: '10px',
-      color: 'white',
+      padding: "15px 30px",
+      fontSize: "18px",
+      cursor: "pointer",
+      border: "none",
+      borderRadius: "5px",
+      margin: "10px",
+      color: "white",
       ...buttonAnimation,
-      width: '40%',
-      transition: 'background-color 0.3s',
-      pointerEvents: habilitarSelecciones ? 'auto' : 'none',
-      '&:hover': {
-        backgroundColor: '#555',
+      width: "40%",
+      transition: "background-color 0.3s",
+      pointerEvents: habilitarSelecciones ? "auto" : "none",
+      "&:hover": {
+        backgroundColor: "#555",
       },
     },
     skipButton: {
-      padding: '10px 20px',
-      fontSize: '16px',
-      cursor: 'pointer',
-      border: 'none',
-      borderRadius: '5px',
-      margin: '10px',
-      backgroundColor: '#2196f3',
-      color: 'white',
+      padding: "10px 20px",
+      fontSize: "16px",
+      cursor: "pointer",
+      border: "none",
+      borderRadius: "5px",
+      margin: "10px",
+      backgroundColor: "#2196f3",
+      color: "white",
     },
     animacionRespuesta: {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      textAlign: 'center',
-      pointerEvents: 'none',
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      textAlign: "center",
+      pointerEvents: "none",
       ...animacionRespuesta,
-      fontSize: '1.5em', // Tamaño de fuente aumentado
+      fontSize: "1.5em",
     },
     resumenContainer: {
-      marginTop: '20px',
-      textAlign: 'center',
+      marginTop: "20px",
+      textAlign: "center",
     },
     resumenContent: {
-      backgroundColor: '#444',
-      padding: '20px',
-      borderRadius: '8px',
+      backgroundColor: "#444",
+      padding: "20px",
+      borderRadius: "8px",
     },
   };
 
+  const iniciarNuevoJuego = () => {
+    ejercicios.forEach((ejercicio) => {
+      ejercicio.respuestas.sort(() => Math.random() - 0.5);
+    });
+
+    setRespuestaCorrecta(null);
+    setSeleccionada(null);
+    setHabilitarSelecciones(true);
+    setEjercicioActual(1);
+    setRespuestaCorrecta(ejercicios[0].respuestaCorrecta);
+  };
+
   useEffect(() => {
-    setTitleAnimation({ opacity: 1, transform: 'translateY(0)' });
-    iniciarNuevoEjercicio();
+    setTitleAnimation({ opacity: 1, transform: "translateY(0)" });
+    iniciarNuevoJuego();
   }, [setTitleAnimation]);
 
   return (
@@ -261,6 +277,7 @@ const MathExercise = () => {
 
       {respuestaCorrecta !== null && (
         <>
+          <h1> {ejercicios[ejercicioActual - 1].nombre}</h1>
           <animated.img
             src={ejercicios[ejercicioActual - 1].imagen}
             alt={`Ejercicio Matemático ${ejercicioActual}`}
@@ -268,6 +285,8 @@ const MathExercise = () => {
           />
 
           <div style={styles.optionsContainer}>
+
+        
             {ejercicios[ejercicioActual - 1].respuestas.map((opcion) => (
               <animated.button
                 key={opcion}
@@ -279,7 +298,7 @@ const MathExercise = () => {
                   handleButtonClick(opcion);
                 }}
               >
-                Opción {opcion}
+                {opcion}
               </animated.button>
             ))}
           </div>
@@ -297,7 +316,8 @@ const MathExercise = () => {
             <p>Respuestas Correctas: {respuestasCorrectas}</p>
             <p>Respuestas Incorrectas: {respuestasIncorrectas}</p>
             <p>
-              Promedio: {((respuestasCorrectas / ejercicios.length) * 100).toFixed(2)}%
+              Promedio:{" "}
+              {((respuestasCorrectas / ejercicios.length) * 100).toFixed(2)}%
             </p>
             <p>Categoría: {obtenerPuntuacionCategoria()}</p>
           </div>
@@ -308,18 +328,17 @@ const MathExercise = () => {
         {respuestaCorrecta !== null && (
           <p
             style={{
-              color: respuestaCorrecta === seleccionada ? 'green' : 'red',
+              color: respuestaCorrecta === seleccionada ? "green" : "red",
             }}
           >
             {respuestaCorrecta === seleccionada
-              ? '¡Correcto!'
-              : 'Incorrecto. Inténtalo de nuevo.'}
+              ? "¡Correcto!"
+              : "Incorrecto. Inténtalo de nuevo."}
           </p>
         )}
       </animated.div>
     </div>
   );
-}
+};
 
 export default MathExercise;
-
